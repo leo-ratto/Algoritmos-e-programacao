e2cb74332c3ef6c9b9c4998e86a7eaf4fd46043a
leo-ratto <leonardo.pace.2007@gmail.com>
2026-04-25 14:48:36 -0300

    👍

--- aula09 - estruturas de repeticao.py
+++ aula09 - estruturas de repeticao.py
@@ -39,17 +39,86 @@ for i in range(2, n+1):
 print(fatorial[n])
 '''
 # Exercício 3
-
+'''
 n = int(input('Insira o valor dos lados do quadrado (entre 1 e 20): '))
 
 while n < 1 or n > 20:
     print('\nValor inválido. Insira um valor entre 1 e 20')
 
-    n = int(input('Insira o valor dos lados do quadrado (entre 1 e 20): '))
+    n = int(input('\nInsira o valor dos lados do quadrado (entre 1 e 20): '))
 
 for i in range(n):
-    if i == 0 or i == n-1:
-        print('*' * n)
+    
+    for r in range(n):
+        
+        if i == 0 or i == (n-1):
+            if r == (n-1):
+                print('*')
+                
+            else:
+                print('*', end='')
+            
+        else:
+            
+            if r == 0:
+                print('*', end='')
+                
+            elif r == (n-1):
+                print('*')
+                
+            else:
+                print(' ', end='')
+'''          
+# Exercício 4
+'''
+n = int(input('Insira o valor da altura do triângulo: '))
 
+for i in range(1, n+1):
+    print('*' * i)
+'''
+# Exercício 5
+'''
+n = int(input('Insira o valor de n: '))
+
+fatorial = 1
+
+soma = 1
+
+for i in range(1, n+1):
+    
+    fatorial *= i
+    
+    soma += (1/fatorial)
+    
+print(f'O valor de E é {soma}')
+'''
+# Exercício 6
+'''
+x = int(input('Insira o valor de x: '))
+
+soma = x**25
+
+inverso = 24
+
+for i in range(2, 26):
+    
+    if i % 2 != 0:   
+        soma += (x**inverso)/(i)
+        
     else:
-        print('*', ' ' * (n-4), '*')
+        soma -= (x**inverso)/(i)
+        
+    inverso -= 1       
+            
+print(f'O valor da somatória é {soma}')
+'''
+# Exercício 7
+'''
+soma = 0
+
+for i in range(1, 51):
+    
+    soma += ((2 * i) - 1) / i
+
+print(f'Soma: {soma}')
+'''
--- teste.py
+++
@@ -1,8 +0,0 @@
-import random
-
-a = 1
-b = 2
-c = 3
-d = 4
-
-print(max(a, b, c, d))
